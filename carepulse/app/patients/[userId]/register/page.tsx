963990aa31c5a4302debb0ed950c9df4dfa6834f
magizh-tech <165991142+magizh-tech@users.noreply.github.com>
2024-08-01 07:52:46 +0530

    1.23

--- carepulse/app/patients/[userId]/register/page.tsx
+++ carepulse/app/patients/[userId]/register/page.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Register = () => {
   return (
-    <div>
+    <div className='flex h-screen max-h-screen'>
+      <section className='remove-scrollbar container'>
+        <div className="sub-container max-2-[860px] flex-1 flex-col py-10">
+          <Image 
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="carepulse"
+            className="mb-12 h-10 w-fit"
+            />
+        </div>
+
+      </section>
       register
     </div>
   )
